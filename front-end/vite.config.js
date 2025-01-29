5e76d6ac51a322909118b99cca27b79aee2059d3
Njau-dev <jeffnjau01@gmail.com>
2025-01-29 17:28:20 +0300

    configuration 2

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// Fix __dirname for ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './src/assets')
     }
   },
