a67028613a2cffa7f914e8e72839f1b6ee90432d
Njau-dev <jeffnjau01@gmail.com>
2025-01-29 18:48:55 +0300

    package.json fixed

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-// Fix __dirname for ES modules
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, './src/assets')
-    }
-  },
   build: {
-    assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg']
+    assetsInlineLimit: 0
   }
 })
